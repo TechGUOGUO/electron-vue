564c1aa25d7c2b29aad2748fa52e2de6858fbd71
GuoYingxu <33899027+GuoYingxu@users.noreply.github.com>
2019-11-04 02:02:54 +0800

    @

--- src/components/VideoPage.vue
+++ src/components/VideoPage.vue
@@ -44,6 +44,9 @@ export default {
         }
         console.log(this.url)
         console.log(this.playUrl)
+        setTimeout(()=>{
+            this.full()
+        },200)
     },
     watch:{
         pageParam(val){
@@ -129,6 +132,91 @@ export default {
       }
     },
     methods:{
+         invokeFieldOrMethod :function(element, method) 
+{
+  var usablePrefixMethod;
+  ["webkit", "moz", "ms", "o", ""].forEach(function(prefix) {
+      if (usablePrefixMethod) return;
+      if (prefix === "") {
+          // 无前缀，方法首字母小写
+          method = method.slice(0,1).toLowerCase() + method.slice(1);   
+      }
+      var typePrefixMethod = typeof element[prefix + method];
+      if (typePrefixMethod + "" !== "undefined") {
+          if (typePrefixMethod === "function") {
+              usablePrefixMethod = element[prefix + method]();
+          } else {
+              usablePrefixMethod = element[prefix + method];
+          }
+      }
+  });
+ 
+      return usablePrefixMethod;
+},
+ 
+//進入全屏
+ launchFullscreen(element) 
+  {
+   //此方法不可以在異步任務中執行，否則火狐無法全屏
+    if(element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if(element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if(element.msRequestFullscreen){ 
+      element.msRequestFullscreen();  
+    } else if(element.oRequestFullscreen){
+       element.oRequestFullscreen();
+   }
+   else if(element.webkitRequestFullscreen)
+    {
+      element.webkitRequestFullScreen();
+    }else{
+ 
+    //    var docHtml  = document.documentElement;
+    //    var docBody  = document.body;
+    //    var videobox  = document.getElementById('videobox');
+    //    var  cssText = 'width:100%;height:100%;overflow:hidden;';
+    //    docHtml.style.cssText = cssText;
+    //    docBody.style.cssText = cssText;
+    //    videobox.style.cssText = cssText+';'+'margin:0px;padding:0px;';
+       document.IsFullScreen = true;
+ 
+     }
+  }, 
+  exitFullscreen()
+  {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if(document.oRequestFullscreen){
+           document.oCancelFullScreen();
+       }else if (document.webkitExitFullscreen){
+        document.webkitExitFullscreen();
+      }else{
+       var docHtml  = document.documentElement;
+       var docBody  = document.body;
+       var videobox  = document.getElementById('videobox');
+       docHtml.style.cssText = "";
+       docBody.style.cssText = "";
+       videobox.style.cssText = "";
+       document.IsFullScreen = false;
+   }
+ },
+ full(){
+this.launchFullscreen(document.getElementById('myVideo1')); 
+//    window.setTimeout(function exit(){
+// //檢查瀏覽器是否處於全屏
+// if(invokeFieldOrMethod(document,'FullScreen') || invokeFieldOrMethod(document,'IsFullScreen') || document.IsFullScreen)
+// {
+//     exitFullscreen();
+// }
+//   },5*1000);
+// },false)
+ } 
+   ,
         buttonHandler(e){
              if(e.type=="routeTo"){
                 this.$emit('routeTo',e.options.path)
